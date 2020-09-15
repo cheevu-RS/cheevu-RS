737ffbce0887e1bb3e254f4ece0252f30b8a7d4a
cheevu-RS <ramrsrs99@gmail.com>
2020-09-15 23:26:02 +0530

    [Add] Discord bot approve mechanism

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Hello there! <br>Here's a random dank meme for you from [r/dankmemes](https://reddit.com/r/dankmemes)!<br>
-## My disappointment is immeasurable<br><img src="https://i.redd.it/896sr7bpu4n51.gif" alt="meme" width="300"/>)<br>
-[Link to Comments](https://reddit.com/r/dankmemes/comments/isn82n/my_disappointment_is_immeasurable/)<br>
+##<span style="background-color: #24292e">OC Maymay ♨ </span> I do wanna end up like him<br><img src="https://i.redd.it/nyeoa3jli7n51.jpg" alt="meme" width="300"/>)<br>
+[Link to Comments](https://reddit.com/r/dankmemes/comments/isxxcm/i_do_wanna_end_up_like_him/)<br>
 Memes are updated once a day using Github Actions
--- generateMeme/redditDankMeme.js
+++ generateMeme/redditDankMeme.js
@@ -1,20 +1,76 @@
 const fetch = require('node-fetch');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const fs = require('fs'); 
-const url = 'https://reddit.com/r/dankmemes/top.json';
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        let memes = data.data.children;
-        let chosenOne = Math.floor(Math.random()*memes.length);
-        let chosenMeme = memes[chosenOne].data;
-        let readmeString = `Hello there! <br>Here's a random dank meme for you from [r/dankmemes](https://reddit.com/r/dankmemes)!<br>\r\n## `
-        if(chosenMeme.link_flair_text)
-            readmeString += `<span style="background-color: #24292e">`+ chosenMeme.link_flair_text +`</span> `;
-        readmeString += chosenMeme.title+`<br><img src="`+chosenMeme.url_overridden_by_dest+`" alt="meme" width="300"/>`+`)<br>\r\n`;
-        readmeString += `[Link to Comments](https://reddit.com`+chosenMeme.permalink+`)<br>\r\n`;
-	readmeString += `Memes are updated once a day using Github Actions`;
-        fs.writeFile("./README.md", readmeString, 'utf8',()=>{
-            console.log("README updated successfully")
-        })
+
+let memes;
+let chosenMeme; 
+let chosenOne;
+let channel;
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    
+    // initialise channel
+    channel = client.channels.cache.get('755470352749953094');
+
+    sendMeme();
+});
+
+client.on('message', msg => {
+  if (msg.content === '$help') {
+    msg.channel.send('Hi there, I help you approve your memes!');
+  }
+});
+client.on('messageReactionAdd', (reaction, user) => {
+    console.log(reaction.emoji);
+    if(reaction.emoji.name === "👍") {
+        writeReadme();
+        client.destroy();
+    }
+    else if(reaction.emoji.name === "👎"){
+        memes.splice(chosenOne, 1);
+        sendMeme();
+    }
+});
+
+const sendMeme = () => {
+    if(memes.length == 0){
+        channel.send("You don't like any of the top reddit dank memes");
+        client.destroy();
+    }
+    chosenOne = Math.floor(Math.random()*memes.length);
+    chosenMeme = memes[chosenOne].data;
+    channel.send('Random meme', {files:[chosenMeme.url_overridden_by_dest]});        
+}
+
+const writeReadme = () => {
+    let readmeString = `Hello there! <br>Here's a random dank meme for you from [r/dankmemes](https://reddit.com/r/dankmemes)!<br>\r\n##`
+    if(chosenMeme.link_flair_text)
+        readmeString += `<span style="background-color: #24292e">`+ chosenMeme.link_flair_text +`</span> `;
+    readmeString += chosenMeme.title+`<br><img src="`+chosenMeme.url_overridden_by_dest+`" alt="meme" width="300"/>`+`)<br>\r\n`;
+    readmeString += `[Link to Comments](https://reddit.com`+chosenMeme.permalink+`)<br>\r\n`;
+    readmeString += `Memes are updated once a day using Github Actions`;
+    fs.writeFile("./README.md", readmeString, 'utf8',() => {
+        console.log("README updated successfully")
     })
-    .catch(err => console.log(err))
+}
+
+const init = async () => {
+    // fetch memes from reddit api
+    const url = 'https://reddit.com/r/dankmemes/top.json';
+    const response = await fetch(url)
+    const data = await response.json()
+    memes = await data.data.children
+
+    // remove gifs from list of memes
+    memes.forEach((meme, index) => {
+        if(meme.data.url.indexOf(".gif") > -1){
+            memes.splice(index,1)
+        }
+    })
+
+    // intialize discord bot    
+    client.login('NzU1NDY3MTgwODQxMzY5NjIz.X2Dtow.R1VKnvAg71AXEQv5tOm1MsIOSYc');
+}
+
+init();
