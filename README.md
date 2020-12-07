6be0db7dc8350823b1b0a29689cb32e59a2a4408
cheevu-RS <ramrsrs99@gmail.com>
2020-12-07 20:13:13 +0530

    Add xkcd script

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-Hello there! <br>Here's a random dank meme for you from [r/dankmemes](https://reddit.com/r/dankmemes)!<br>
-## <span style="background-color: #24292e">I don't have the confidence to choose a funny flair</span> Enforced strictly<br><img src="https://i.redd.it/84njpfim0d261.jpg" alt="meme" width="300"/>)<br>
-[Link to Comments](https://reddit.com/r/dankmemes/comments/k3ttmf/enforced_strictly/)<br>
-Memes are updated once a day using Github Actions
+Hello there! <br>Here's a random xkcd comic!<br>
+## <img src="https://imgs.xkcd.com/comics/lojban.png" alt="meme" width="300"/>)<br>
+Comics are updated once a day using Github Actions
---
+++ generateMeme/randomxkcd.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+const fs = require('fs').promises; 
+
+let comic_url;
+let count;
+let image_url;
+
+
+const writeReadme = async () => {
+    let readmeString = `Hello there! <br>Here's a random xkcd comic!<br>\r\n## `
+    readmeString += `<img src="`+image_url+`" alt="meme" width="300"/>`+`)<br>\r\n`;
+    readmeString += `Comics are updated once a day using Github Actions`;
+    await fs.writeFile("./README.md", readmeString, 'utf8',() => {
+        console.log("README updated successfully")
+    })
+}
+const incrementXkcdCounter = async () => {
+    count = await fs.readFile("./generateMeme/xkcdCounter");
+    count++;
+    await fs.writeFile("./generateMeme/xkcdCounter", count.toString());
+}
+const init = async () => {
+    await incrementXkcdCounter();
+
+    const number = Math.floor(Math.random()*count);
+    const url = 'https://xkcd.com/'+number+"/";
+    const response = await fetch(url)
+    const data = await response.text()
+    const pattern = "hotlinking\/embedding(.*)";
+    image_url = data.match(pattern)[0].split(": ")[1];
+    await writeReadme();
+
+}
+
+init();
